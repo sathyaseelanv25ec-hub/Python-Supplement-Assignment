8b6a2f7d948a4e45770c92fe8874cc84f894a9b5
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 15:02:53 +0530

    Update problem_49.py

--- problem_49.py
+++ problem_49.py
@@ -1,9 +1,7 @@
 # Problem 49: Find all divisors of a number
-# Find and fix the error
-
-def find_divisors(n):
+ def find_divisors(n):
     divisors = []
-    for i in range(1, n):
+    for i in range(1, n + 1):  # include n
         if n % i == 0:
             divisors.append(i)
     return divisors
