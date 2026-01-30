38ecfaf7d05130c9642b51980cc78cda3246b499
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 12:02:19 +0530

    Refactor nested list flattening using list comprehension

--- problem_22.py
+++ problem_22.py
@@ -1,9 +1,8 @@
 # Problem 22: Flatten a nested list
 # Find and fix the error
 
-nested_list = [[1, 2], [3, 4], [5, 6]]
-flat_list = []
-for sublist in nested_list:
-    for item in sublist:
-        flat_list.append(item)
+ nested_list = [[1, 2], [3, 4], [5, 6]]
+
+flat_list = [item for sublist in nested_list for item in sublist]
+
 print(f"Flattened list: {flat_list}")
