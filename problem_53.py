7bec80c7f1c80761c4ac3ffb8bcc1f6a8c1aa34f
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 15:10:36 +0530

    Update problem_53.py

--- problem_53.py
+++ problem_53.py
@@ -1,8 +1,6 @@
 # Problem 53: Check if two strings are anagrams
-# Find and fix the error
-
-def are_anagrams(str1, str2):
-    return sorted(str1) == sorted(str2)
+ def are_anagrams(str1, str2):
+    return sorted(str1.lower()) == sorted(str2.lower())
 
 word1 = "Listen"
 word2 = "Silent"
