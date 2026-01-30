9844cf4c461931996548ba30529115ed202c360a
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 14:28:08 +0530

    Fix add_item function to use append method

--- problem_26.py
+++ problem_26.py
@@ -1,9 +1,8 @@
 # Problem 26: Add item to a list
 # Find and fix the error
 
-def add_item(lst, item):
-    lst = lst + [item]
-    return lst
+ def add_item(lst, item):
+    lst.append(item)
 
 my_list = [1, 2, 3]
 add_item(my_list, 4)
