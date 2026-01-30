ce94a5d4a14c1ecf568b56abec141ee48e328463
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 11:49:42 +0530

    Update problem_14.py

--- problem_14.py
+++ problem_14.py
@@ -1,10 +1,12 @@
 # Problem 14: Check if a number is prime
 # Find and fix the error
 
+ import math
+
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, n):
+    for i in range(2, int(math.isqrt(n)) + 1):  # check up to √n
         if n % i == 0:
             return False
     return True
