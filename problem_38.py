32e19290e712fea7186224022567ae4d9ad9b31e
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 14:43:23 +0530

    Fix integer division in sum_of_digits function

--- problem_38.py
+++ problem_38.py
@@ -1,12 +1,12 @@
 # Problem 38: Find sum of digits
 # Find and fix the error
 
-def sum_of_digits(n):
+ def sum_of_digits(n):
     total = 0
     while n > 0:
         digit = n % 10
         total += digit
-        n = n / 10
+        n = n // 10   # integer division
     return total
 
 print(f"Sum of digits of 12345: {sum_of_digits(12345)}")
