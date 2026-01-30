1b4506efb376fe8b7b391c66046aeeff429bd447
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 11:31:46 +0530

    Correct comparison operator in even/odd check
    Fix comparison operator for checking even or odd.

--- problem_03.py
+++ problem_03.py
@@ -1,8 +1,8 @@
 # Problem 3: Check if a number is even or odd
 # Find and fix the error
 
-number = 15
-if number % 2 = 0:
+ number = 15
+if number % 2 == 0:  # use '==' for comparison
     print("Even")
 else:
     print("Odd")
