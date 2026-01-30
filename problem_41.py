b4426abaf4e810dd439b47db6e59f2fb92c042e0
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 14:47:53 +0530

    Fix indentation and add break statement in loop

--- problem_41.py
+++ problem_41.py
@@ -1,10 +1,13 @@
 # Problem 41: Find index of element in list
 # Find and fix the error
 
-numbers = [10, 20, 30, 40, 50]
+ numbers = [10, 20, 30, 40, 50]
 search = 30
 index = -1
+
 for i in range(len(numbers)):
     if numbers[i] == search:
         index = i
+        break  # stop once found
+
 print(f"Index of {search}: {index}")
