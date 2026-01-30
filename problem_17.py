7a4dc40a2a93239f526f013df54610680f2fb8e5
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 11:54:54 +0530

    Refactor capitalize_words function for brevity

--- problem_17.py
+++ problem_17.py
@@ -1,12 +1,8 @@
 # Problem 17: Capitalize first letter of each word
 # Find and fix the error
 
-def capitalize_words(text):
-    words = text.split()
-    capitalized = []
-    for word in words:
-        capitalized.append(word.capitalize())
-    return " ".join(capitalized)
+ def capitalize_words(text):
+    return " ".join(word.capitalize() for word in text.split())
 
 sentence = "hello world from python"
 print(capitalize_words(sentence))
