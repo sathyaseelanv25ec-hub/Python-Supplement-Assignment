450ab4ebfe916ecd5ef4f3447fa4e5c3480bf14a
sathyaseelanv25ec-hub <sathyaseelan.v_25ec@kgkite.ac.in>
2026-01-30 12:01:00 +0530

    Fix indentation in is_sorted function
    Fixed indentation for the is_sorted function.

--- problem_21.py
+++ problem_21.py
@@ -1,7 +1,7 @@
 # Problem 21: Check if list is sorted
 # Find and fix the error
 
-def is_sorted(lst):
+ def is_sorted(lst):
     for i in range(len(lst) - 1):
         if lst[i] > lst[i + 1]:
             return False
